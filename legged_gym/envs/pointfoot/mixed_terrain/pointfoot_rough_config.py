c3a9c8ab96451c8db3a580a028dca2b1cc323e14
peachvegetable <yihang.cai@mail.utoronto.ca>
2024-06-20 00:20:09 -0400

    num_env=1

--- legged_gym/envs/pointfoot/mixed_terrain/pointfoot_rough_config.py
+++ legged_gym/envs/pointfoot/mixed_terrain/pointfoot_rough_config.py
@@ -2,7 +2,7 @@ from legged_gym.envs.base.base_config import BaseConfig
 
 class PointFootRoughCfg(BaseConfig):
     class env:
-        num_envs = 8192
+        num_envs = 1
         num_propriceptive_obs = 27
         num_privileged_obs = 148  # if not None a priviledge_obs_buf will be returned by step() (critic obs for assymetric training). None is returned otherwise
         num_actions = 6
